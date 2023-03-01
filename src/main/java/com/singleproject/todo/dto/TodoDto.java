d0f8d206db851a11e190f58aabd53aecd5c602e4
sugyeonghh <sugyeong.py@gmail.com>
2023-03-01 23:19:54 +0900

    Dto 클래스 합치기, responseDto 추가

---
+++ src/main/java/com/singleproject/todo/dto/TodoDto.java
@@ -0,0 +1,49 @@
+package com.singleproject.todo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+public class TodoDto {
+	@Getter
+	public static class Post {
+		@NotBlank
+		private String title;
+
+		private int todoOrder;
+
+		private boolean completed;
+	}
+
+	@Getter
+	@Setter
+	public static class Patch {
+		private int todoId;
+
+		private String title;
+
+		private int todoOrder;
+
+		private boolean completed;
+	}
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	public static class Response {
+		private int todoId;
+
+		private String title;
+
+		private int todoOrder;
+
+		private boolean completed;
+
+		private LocalDateTime createdAt;
+
+		private LocalDateTime modifiedAt;
+	}
+}
