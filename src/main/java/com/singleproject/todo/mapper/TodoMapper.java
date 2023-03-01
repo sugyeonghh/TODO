f87b07353c18c95928ae8d8d491653b8071883a4
sugyeonghh <sugyeong.py@gmail.com>
2023-03-01 23:21:19 +0900

    responseDto로 변환하는 mapper 추가

--- src/main/java/com/singleproject/todo/mapper/TodoMapper.java
+++ src/main/java/com/singleproject/todo/mapper/TodoMapper.java
@@ -1,14 +1,18 @@
 package com.singleproject.todo.mapper;
 
-import com.singleproject.todo.dto.TodoPatchDto;
-import com.singleproject.todo.dto.TodoPostDto;
+import com.singleproject.todo.dto.TodoDto;
 import com.singleproject.todo.entity.Todos;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TodoMapper {
-	Todos TodoPostDtoToTodo(TodoPostDto todoPostDto);
+	Todos TodoPostDtoToTodo(TodoDto.Post todoPostDto);
 
-	Todos TodoPatchDtoToTodo(TodoPatchDto todoPatchDto);
+	Todos TodoPatchDtoToTodo(TodoDto.Patch todoPatchDto);
 
+	TodoDto.Response TodoToTodoResponseDto(Todos todo);
+
+	List<TodoDto.Response> TodosToTodoResponseDtos(List<Todos> todo);
 }
